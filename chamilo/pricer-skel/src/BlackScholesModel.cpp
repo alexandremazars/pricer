4046ace17d65c69c37e3f755b906e6690d414de3
Daniella Teukeng Mobou <Daniella.Teukeng-Mobou@grenoble-inp.org>
2018-09-18 13:23:21 +0200

    prix à tout instant t

--- chamilo/pricer-skel/src/BlackScholesModel.cpp
+++ chamilo/pricer-skel/src/BlackScholesModel.cpp
@@ -67,3 +67,58 @@ void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *r
     pnl_vect_free(&G);
     pnl_rng_free(&rng);
 }
+
+void BlackScholesModel::asset(PnlMat *path, double t, double T, int nbTimeSteps, PnlRng *rng, const PnlMat *past){
+    double delta_t = (nbTimeSteps + 1)/T;
+    //Remplir la mtrice de correlation
+    PnlMat *mat_Cor = pnl_mat_create_from_scalar(size_, size_, rho_ );
+    for (int i = 0; i < size_; ++i) {
+        pnl_mat_set(mat_Cor, i, i, 1);
+    }
+
+    //Matrice de Cholesky
+    PnlMat *mat_Chol = pnl_mat_copy(mat_Cor);
+    pnl_mat_chol(mat_Chol);
+
+    pnl_mat_resize(path, nbTimeSteps + 1, size_);
+
+    //simuler les vecteurs gaussien
+    PnlMat *suite_Gauss = pnl_mat_create(size_, nbTimeSteps + 1);
+    PnlVect *G = pnl_vect_new();
+    for (int d = 0; d < size_; ++d) {
+        G = pnl_vect_new();
+        pnl_rng_init(rng, PNL_RNG_MERSENNE);
+        pnl_rng_sseed(rng, time(NULL));
+        pnl_vect_rng_normal(G, nbTimeSteps - t, rng);
+        pnl_mat_set_row(suite_Gauss, G, d);
+    }
+    //Calcul des prix
+    PnlVect *row_Chol;
+    PnlVect *col_Gauss;
+
+    //mettre dans path les valeurs de past
+    for (int j = 0; j <= t; ++j) {
+        PnlVect *price_i;
+        pnl_mat_get_row(price_i, past, j);
+        pnl_mat_set_row(path, price_i, j);
+    }
+
+    //Simulées les valeurs suivantes
+
+    for (int d = 0; d < size_ ; ++d) {
+        double prix_Prec = pnl_mat_get(past,t,d);
+        double prix = prix_Prec;
+        double sigma = pnl_vect_get(sigma_, d);
+        pnl_mat_get_row(row_Chol, mat_Chol, d);
+
+        for (int i = 0; i < nbTimeSteps - t ; ++i) {
+            pnl_mat_get_col(col_Gauss, suite_Gauss, i);
+            pnl_vect_mult_vect_term(row_Chol, col_Gauss);
+            prix = exp((r_-(pow(sigma,2))/2)/delta_t + pnl_vect_get(row_Chol, 0) * sigma * sqrt(delta_t));
+            prix = prix * prix_Prec;
+            pnl_mat_set(path, (i+t+1), d, prix);
+        }
+    }
+
+
+}
