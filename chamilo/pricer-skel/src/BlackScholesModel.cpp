fe906c705cf30129ed490c4225c02c9d016c0971
Mylene Le Calvez <mylene.le-calvez@grenoble-inp.org>
2018-09-18 10:25:15 +0200

    modifs asset

--- chamilo/pricer-skel/src/BlackScholesModel.cpp
+++ chamilo/pricer-skel/src/BlackScholesModel.cpp
@@ -14,12 +14,56 @@
      */
 void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *rng)
 {
-    pnl_mat_resize(path, nbTimeSteps + 1, size_);
-    PnlVect *G = pnl_vect_new();
-    pnl_rng_init(rng, PNL_RNG_MERSENNE);
-    pnl_rng_sseed(rng, time(NULL));
-    pnl_vect_rng_normal(G, nbTimeSteps + 1, rng);
+    double delta_t = (nbTimeSteps + 1)/T;
 
+    //Remplir la mtrice de correlation
+    PnlMat *mat_Cor = pnl_mat_create_from_scalar(size_, size_, rho_ );
+    for (int i = 0; i < size_; ++i) {
+        pnl_mat_set(mat_Cor, i, i, 1);
+    }
+
+    //Matrice de Cholesky
+    PnlMat *mat_Chol = pnl_mat_copy(mat_Cor);
+    pnl_mat_chol(mat_Chol);
+
+    pnl_mat_resize(path, nbTimeSteps + 1, size_);
+
+    //Simuler vecteurs gaussiens
+    PnlMat *suite_Gauss = pnl_mat_create(size_, nbTimeSteps + 1);
+    PnlVect *G = pnl_vect_new();
+    for (int d = 0; d < size_; ++d) {
+        G = pnl_vect_new();
+        pnl_rng_init(rng, PNL_RNG_MERSENNE);
+        pnl_rng_sseed(rng, time(NULL));
+        pnl_vect_rng_normal(G, nbTimeSteps + 1, rng);
+        pnl_mat_set_row(suite_Gauss, G, d);
+    }
+
+
+    //Calcul des prix
+    PnlVect *row_Chol;
+    PnlVect *col_Gauss;
+    for (int d = 0; d < size_ ; ++d) {
+        double prix_Prec = pnl_vect_get(spot_, d);
+        double prix = prix_Prec;
+        pnl_mat_set(path, 0, d, prix_Prec);
+        double sigma = pnl_vect_get(sigma_, d);
+        pnl_mat_get_row(row_Chol, mat_Chol, d);
+        for (int i = 1; i < nbTimeSteps+1; ++i) {
+            pnl_mat_get_col(col_Gauss, suite_Gauss, i);
+            pnl_vect_mult_vect_term(row_Chol, col_Gauss);
+            prix = prix_Prec * exp((r_-(pow(sigma,2))/2)/delta_t + pnl_vect_get(row_Chol, 0) * sigma * sqrt(delta_t));
+            pnl_mat_set(path, i, d, prix);
+            prix_Prec = prix;
+        }
+    }
+
+    // Free
+    pnl_vect_free(&row_Chol);
+    pnl_vect_free(&col_Gauss);
+    pnl_mat_free(&suite_Gauss);
+    pnl_mat_free(&mat_Chol);
+    pnl_mat_free(&mat_Cor);
     pnl_vect_free(&G);
     pnl_rng_free(&rng);
 }
