031f653e66f1b5dfd0a63bde7fec02beea12fb1a
Daniella Teukeng Mobou <Daniella.Teukeng-Mobou@grenoble-inp.org>
2018-09-18 17:45:08 +0200

    ajout de dossier test

---
+++ chamilo/pricer-skel/tests/test_price.cpp
@@ -0,0 +1,17 @@
+//
+// Created by teukengd on 9/14/18.
+//
+
+#include "gtest/gtest.h"
+#include "..src/MonteCarlo.hpp"
+
+using namespace std;
+
+TEST(MonteCarlo, Price){
+    EXPECT_EQ(,) << "Error : message d'erreur";
+}
+
+int main(int argc, char **argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
