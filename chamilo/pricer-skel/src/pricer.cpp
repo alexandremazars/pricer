70fd48b2091c916df9725bd0451183d4ce7596e8
Alexandre <alexandre.mazars@grenoble-inp.org>
2018-09-21 17:51:59 +0200

    pricer marche

--- chamilo/pricer-skel/src/pricer.cpp
+++ chamilo/pricer-skel/src/pricer.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
 #include "jlparser/parser.hpp"
+#include "BlackScholesModel.hpp"
+#include "MonteCarlo.hpp"
+#include "AsianOption.hpp"
+#include "BasketOption.hpp"
+#include "CallOption.hpp"
+#include "PerformanceOption.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    double T, r, strike;
+    double fdStep = 0.1;
+    double T, r, strike, correlation;
     PnlVect *spot, *sigma, *divid;
     string type;
-    int size;
+    int size, timestep;
     size_t n_samples;
 
     char *infile = argv[1];
@@ -28,21 +35,49 @@ int main(int argc, char **argv)
     if (type != "performance"){
         P->extract("strike", strike);
     }
+    P->extract("timestep number", timestep);    
     P->extract("sample number", n_samples);
 
+    Option* opt;
+    
     BlackScholesModel *bsmodel = new BlackScholesModel(size, r, correlation, sigma, spot);
     if (type == "asian"){
-        Option *option = new AsianOption(T, timestep, size, strike);        
+        opt = new AsianOption(T, timestep, size, strike);        
     } else if ( type == "basket"){
-        Option *option = new BasketOption(T, timestep, size, strike);   
+        opt = new BasketOption(T, timestep, size, strike);   
     } else if ( type == "performance"){
-        Option *option = new PerformanceOption(T, timestep, size, strike);        
+        opt = new PerformanceOption(T, timestep, size);        
     }
     PnlRng *rng= pnl_rng_create(PNL_RNG_MERSENNE);
+    
     //
     pnl_rng_init(rng, PNL_RNG_MERSENNE);
     pnl_rng_sseed(rng, time(NULL));
-    MonteCarlo *mCarlo = new MonteCarlo(bsmodel, option, rng, fdStep, n_samples);
+
+    
+    MonteCarlo *mCarlo = new MonteCarlo(bsmodel, opt, rng, fdStep, n_samples);
+
+
+    double prix = 0.0;
+    double ic = 0.0;
+    mCarlo->price(prix , ic);
+    printf("============== \nPrix: %f \nIc: %f \n==============\n", prix, ic);
+
+    PnlMat *past = pnl_mat_create_from_scalar(1, size, 100);
+    PnlVect *delta = pnl_vect_create(size);
+    PnlVect *conf_delta = pnl_vect_create(size);
+
+    mCarlo->delta(past, 0, delta, conf_delta);
+    for (int i =0; i < size; i ++){
+        printf("Delta actif %u: %f\n", i+1, pnl_vect_get(delta, i));
+        printf("Standard Deviation actif %u: %f\n", i+1, pnl_vect_get(conf_delta, i));
+    }
+
+
+    pnl_mat_free(&past);
+    pnl_vect_free(&delta);
+    pnl_vect_free(&conf_delta);
+
 
 
     delete P;
